b280d8f62a21310d4c8c5056e47cefe99c93356c
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-13 17:17:03 +0100

    Add functionality to render a list of cards

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Card from "../Card/Card"
+import './CardList.scss'
 
-const CardList = () => {
+const CardList = (props) => {
+
+  const {beers} = props
+
   return (
-    <div>CardList</div>
+    <div className='card-list'>
+      {beers.map(beer => {
+        return <Card key={beer.id} name={beer.name} tagline={beer.tagline} description={beer.description} image={beer.image_url} abv={beer.abv} />
+      })}
+    </div>
   )
 }
 
