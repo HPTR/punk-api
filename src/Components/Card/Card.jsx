2653262d75cc8b4f3fa74ee287288a7fc9744124
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-13 17:16:34 +0100

    Add basic card JSX and styling

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Card.scss'
 
-const Card = () => {
+const Card = (props) => {
+
+  const {
+    name,
+    tagline,
+    description,
+    image,
+    abv
+  } = props;
+
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img className='card__image' src={image} alt={name} />
+      <h2 className='card__name'>{name}</h2>
+      <h3 className='card__tagline'>{tagline}</h3>
+      <p className='card__description'>{description}</p>
+      <p className='card__abv'>{abv}</p>
+    </div>
   )
 }
 
