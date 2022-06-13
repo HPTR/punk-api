3c927700c79dd60821c90afb60576fd6b34cd580
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-13 16:02:18 +0100

    Set states to be passed as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css';
-import {beers} from "./data/beers";
+import { useState } from 'react';
+import './App.scss';
+import beers from "./data/beers";
+
+import NavBar from "./containers/NavBar/NavBar";
+import BeerDisplay from "./containers/BeerDisplay/BeerDisplay";
 
 function App() {
+
+  const [filterQuery, setfilterQuery] = useState([])
+  const [searchCriteria, setsearchCriteria] = useState("");
+  const [filters, setfilters] = useState(['High ABV (> 6.0%)', 'Classic Range', 'Acidic (ph < 4)'])
+
+  const filteredBeers = beers.filter(beer => {
+    return beer
+  });
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar filters={filters} />
       <BeerDisplay />
     </div>
   );
