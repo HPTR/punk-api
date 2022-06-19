cea796efe9bd214a4f68f16efc72dc0967e02fd2
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-19 14:47:39 +0100

    Add BEM class names and first brewed JSX

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -9,17 +9,42 @@ const Card = (props) => {
     description,
     image,
     abv,
-    ph
+    ph,
+    first_brewed
   } = props;
+
+  let truncatedDescription
+
+  if (description[49] === ' ') {
+    truncatedDescription = description.substr(0, 49) + '...';
+  } else {
+    truncatedDescription = description.substr(0, 50) + '...';
+  }
+
+
 
   return (
     <div className='card'>
       <img className='card__image' src={image} alt={name} />
-      <h2 className='card__name'>{name}</h2>
-      <h3 className='card__tagline'>{tagline}</h3>
-      <p className='card__description'>{description}</p>
-      <p className='card__abv'>{abv}</p>
-      <p>{ph}</p>
+      <div className="card__heading">
+        <h2 className='card__name'>{name}</h2>
+        <h3 className='card__tagline'>{tagline}</h3>
+      </div>
+      {/* <p className='card__description'>{truncatedDescription}</p> */}
+      <div className="card__stats">
+        <div className="card__stat separator">
+          <p className='card__stats--title'>ABV</p>
+          <p className='card__stats--info'>{abv}</p>
+        </div>
+        <div className="card__stat separator">
+          <p className='card__stats--title'>Ph</p>
+          <p className='card__stats--info'>{ph}</p>
+        </div>
+        <div className="card__stat">
+          <p className='card__stats--title'>First Brewed</p>
+          <p className='card__stats--info'>{first_brewed}</p>
+        </div>
+      </div>
     </div>
   )
 }
