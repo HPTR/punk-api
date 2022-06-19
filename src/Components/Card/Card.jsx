e43e68353425d2b82113a91c8b2c6e77a79c1342
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-19 23:47:19 +0100

    Adjust card JSX for hover styling

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -25,25 +25,30 @@ const Card = (props) => {
 
   return (
     <div className='card'>
-      <img className='card__image' src={image} alt={name} />
-      <div className="card__heading">
-        <h2 className='card__name'>{name}</h2>
-        <h3 className='card__tagline'>{tagline}</h3>
+        <img className='card__image' src={image ? image : 'http://placehold.jp/c7c7c7/000000/100x200.png?text=No%20Image%20Found'} alt={name} />
+        <div className="card__large-card-content">
+          <div className="card__heading">
+            <h2 className='card__name'>{name}</h2>
+            <h3 className='card__tagline'>{tagline}</h3>
+          </div>
+                {/* <p className='card__description'>{truncatedDescription}</p> */}
+                <div className="card__stats">
+          <div className="card__stat separator">
+            <p className='card__stats--title'>ABV</p>
+            <p className='card__stats--info'>{abv ? abv + '%' : 'Unknown'}</p>
+          </div>
+          <div className="card__stat separator">
+            <p className='card__stats--title'>Ph</p>
+            <p className='card__stats--info'>{ph ? ph : 'Unknown'}</p>
+          </div>
+          <div className="card__stat">
+            <p className='card__stats--title'>First Brewed</p>
+            <p className='card__stats--info'>{first_brewed ? first_brewed : 'Unknown'}</p>
+          </div>
+        </div>
       </div>
-      {/* <p className='card__description'>{truncatedDescription}</p> */}
-      <div className="card__stats">
-        <div className="card__stat separator">
-          <p className='card__stats--title'>ABV</p>
-          <p className='card__stats--info'>{abv}</p>
-        </div>
-        <div className="card__stat separator">
-          <p className='card__stats--title'>Ph</p>
-          <p className='card__stats--info'>{ph}</p>
-        </div>
-        <div className="card__stat">
-          <p className='card__stats--title'>First Brewed</p>
-          <p className='card__stats--info'>{first_brewed}</p>
-        </div>
+      <div className="card__read-more">
+        Read More
       </div>
     </div>
   )
