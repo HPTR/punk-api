e3f0ab738df294e2978208368e7f3e282c575861
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-20 17:07:37 +0100

    Pass id prop to Card component for routing purposes

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -9,7 +9,7 @@ const CardList = (props) => {
   return (
     <div className='card-list'>
       {beers.map(beer => {
-        return <Card key={beer.id} name={beer.name} tagline={beer.tagline} description={beer.description} image={beer.image_url} abv={beer.abv} ph={beer.ph} first_brewed={beer.first_brewed} />
+        return <Card key={beer.id} id={beer.id} name={beer.name} tagline={beer.tagline} description={beer.description} image={beer.image_url} abv={beer.abv} ph={beer.ph} first_brewed={beer.first_brewed} />
       })}
     </div>
   )
