eaa73361fa9e718653ffb3fd175f9623d300f999
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-21 23:34:50 +0100

    Update JSX formatting and class names

--- src/containers/BeerInfo/BeerInfo.jsx
+++ src/containers/BeerInfo/BeerInfo.jsx
@@ -11,20 +11,32 @@ const BeerInfo = (props) => {
 
     return (
     <div className="beer-info">
-        <Link to="/"><img className="beer-info__back" src={back} alt="Back" /></Link>
+        <Link to="/"><div className='beer-info__back'><img className="beer-info__back--image" src={back} alt="Back" />Back</div></Link>
         <img className='beer-info__image' src={currentBeer.image_url} alt={currentBeer.name} />
-        <div class="beer-info__text">
+        <div className="beer-info__text">
             <h2 className="beer-info__name">{currentBeer.name}</h2>
             <h3 className="beer-info__tagline">{currentBeer.tagline}</h3>
             <p className="beer-info__description">{currentBeer.description}</p>
             <ul className="beer-info__info-list">
-                <li className="beer-info__list-item">Alcohol by Volume (ABV): {currentBeer.abv}</li>
-                <li className="beer-info__list-item">International Bitterness Units (IBU): {currentBeer.ibu}</li>
-                <li className="beer-info__list-item">Colour: {currentBeer.ebc} EBC/ {currentBeer.srm} SRM</li>
-                <li className="beer-info__list-item">Ph: {currentBeer.ph}</li>
+                <li className="beer-info__list-item">
+                    <div className="beer-info__list-item--name">Alcohol by Volume (ABV)</div>
+                    <div className="beer-info__list-item--info">{currentBeer.abv}</div>
+                </li>
+                <li className="beer-info__list-item">
+                    <div className="beer-info__list-item--name">International Bitterness Units (IBU)</div>
+                    <div className="beer-info__list-item--info">{currentBeer.ibu}</div>
+                </li>
+                <li className="beer-info__list-item">
+                    <div className="beer-info__list-item--name">Colour</div>
+                    <div className="beer-info__list-item--info">{currentBeer.ebc} EBC/{currentBeer.srm} SRM</div>
+                </li>
+                <li className="beer-info__list-item">
+                    <div className="beer-info__list-item--name">Ph</div>
+                    <div className="beer-info__list-item--info">{currentBeer.ph}</div>
+                </li>
                 <ul className="beer-info__list">
-                    Food pairing suggestions:
-                    {currentBeer.food_pairing.map(item => <li className="beer-info__list-item">{item}</li>)}
+                    Food pairing suggestions
+                    {currentBeer.food_pairing.map((item, index) => <li key={index} className="beer-info__list-item">{item}</li>)}
                 </ul>
             </ul>
         </div>
