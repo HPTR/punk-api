873beac8835618431b3bf84724dd2f928e17da12
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-22 14:25:08 +0100

    Update formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,74 @@
-import { useState, useEffect } from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import './App.scss';
-import './assets/sass/reset.scss'
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import "./assets/sass/reset.scss";
 
 import NavBar from "./containers/NavBar/NavBar";
 import CardList from "./components/CardList/CardList";
-import BeerInfo from './containers/BeerInfo/BeerInfo';
+import BeerInfo from "./containers/BeerInfo/BeerInfo";
 
 function App() {
-
-  const [beers, setBeers] = useState([])
+  const [beers, setBeers] = useState([]);
   const [searchCriteria, setSearchCriteria] = useState("");
-  const filterLabels = [{
-    label: 'High ABV (> 6.0%)',
-    filter: 'abv'
-  },
-  {
-    label: 'Classic Range',
-    filter: 'first_brewed'
-  },
-  {
-    label: 'Acidic (Ph < 4)',
-    filter: 'ph'
-  }];
+  const filterLabels = [
+    {
+      label: "High ABV (> 6.0%)",
+      filter: "abv",
+    },
+    {
+      label: "Classic Range",
+      filter: "first_brewed",
+    },
+    {
+      label: "Acidic (Ph < 4)",
+      filter: "ph",
+    },
+  ];
   const [filteredBeers, setFilteredBeers] = useState([]);
   const [filters, setFilters] = useState({
-    abv: '',
-    first_brewed: '',
-    ph: ''
-  })
+    abv: "",
+    first_brewed: "",
+    ph: "",
+  });
 
   const getBeers = async () => {
     let allBeers = [];
     for (let i = 1; i < 6; i++) {
-      const beersArr = await fetch(`https://api.punkapi.com/v2/beers?per_page=65&page=${i}`)
-      .then(response => response.json())
-      .then(data => data);
-      beersArr.forEach(beer => allBeers.push(beer))
+      const beersArr = await fetch(
+        `https://api.punkapi.com/v2/beers?per_page=65&page=${i}`
+      )
+        .then((response) => response.json())
+        .then((data) => data);
+      beersArr.forEach((beer) => allBeers.push(beer));
     }
-    setBeers(allBeers)
-    setFilteredBeers(allBeers)
-  }
+    setBeers(allBeers);
+    setFilteredBeers(allBeers);
+  };
 
   useEffect(() => {
-    getBeers()
+    getBeers();
   }, []);
 
   useEffect(() => {
-    setFilteredBeers(filterArray(beers, filters))
-  }, [filters, searchCriteria])
+    setFilteredBeers(filterArray(beers, filters));
+  }, [filters, searchCriteria]);
 
   const handleCheck = (event) => {
     const selectedFilter = event.target.dataset.filter;
-    const updatedState = {...filters};
+    const updatedState = { ...filters };
 
     if (event.target.checked) {
       switch (selectedFilter) {
-        case 'abv':
-          updatedState.abv = abv => parseFloat(abv) > 6;
+        case "abv":
+          updatedState.abv = (abv) => parseFloat(abv) > 6;
           setFilters(updatedState);
           break;
-        case 'first_brewed':
-          updatedState.first_brewed = first_brewed => first_brewed[5] === '0';
+        case "first_brewed":
+          updatedState.first_brewed = (first_brewed) => first_brewed[5] === "0";
           setFilters(updatedState);
           break;
-        case 'ph':
-          updatedState.ph = ph => parseFloat(ph) < 4;
+        case "ph":
+          updatedState.ph = (ph) => parseFloat(ph) < 4;
           setFilters(updatedState);
           break;
         default:
@@ -73,16 +76,16 @@ function App() {
       }
     } else {
       switch (selectedFilter) {
-        case 'abv':
-          updatedState.abv = '';
+        case "abv":
+          updatedState.abv = "";
           setFilters(updatedState);
           break;
-        case 'first_brewed':
-          updatedState.first_brewed = '';
+        case "first_brewed":
+          updatedState.first_brewed = "";
           setFilters(updatedState);
           break;
-        case 'ph':
-          updatedState.ph = '';
+        case "ph":
+          updatedState.ph = "";
           setFilters(updatedState);
           break;
         default:
@@ -93,27 +96,32 @@ function App() {
 
   const handleSearchChange = (event) => {
     setSearchCriteria(event.target.value);
-  }
+  };
 
-  function filterArray(array, filters) {
-    const filterKeys = Object.keys(filters);
-    const checkboxFilter =  array.filter(item => {
+  function filterArray(beers, filters) {
+    const filterConditions = Object.keys(filters);
+    const checkboxFilter = beers.filter((item) => {
       // validates all filter criteria
-      return filterKeys.every(key => {
+      return filterConditions.every((key) => {
         // ignores non-function predicates
-        if (typeof filters[key] !== 'function') return true;
+        if (typeof filters[key] !== "function") return true;
         return filters[key](item[key]);
       });
     });
-    return checkboxFilter.filter(beer => beer.name.toLowerCase().includes(searchCriteria.toLowerCase()))
+    return checkboxFilter.filter((beer) =>
+      beer.name.toLowerCase().includes(searchCriteria.toLowerCase())
+    );
   }
-
 
   return (
     <Router>
       <div className="punk-api">
-        <h1 className='punk-api__heading'>Punk API</h1>
-        <NavBar handleCheck={handleCheck} filterLabels={filterLabels} handleSearchChange={handleSearchChange} />
+        <h1 className="punk-api__heading">Punk API</h1>
+        <NavBar
+          handleCheck={handleCheck}
+          filterLabels={filterLabels}
+          handleSearchChange={handleSearchChange}
+        />
         <Routes>
           <Route path="/" element={<CardList beers={filteredBeers} />} />
           <Route path="/beer/:beerId" element={<BeerInfo beers={beers} />} />
